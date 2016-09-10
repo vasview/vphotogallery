dd8630ad2814873b576d6792ed8ea327fc2ae402
Vasily Toropchin <vasview@gmail.com>
2016-09-10 19:13:42 +0600

    Add some seeds.
    Didn't test it.
    I hope it will work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,30 @@ User.create(name: 'Admin', email: 'admin@example.com', password: '123456', passw
 	u.admin = 'false'
 	u.save
 end
+
+users = User.all
+
+def copy_image_fixture(file_name, id)
+  dir = Rails.root.join('public', 'system', 'photos', 'images', '000', '000')
+  Dir.mkdir(dir) unless File.directory?(dir)
+  FileUtils.cp(file_name, dir.join(id.to_s))
+end
+
+fixtures_path = Rails.root.join('app', 'assets', 'images', 'fixtures')
+
+10.times do 
+	photo = Photo.create!(
+	    caption: Faker::Lorem.sentence(1),
+	    user_id: users.id.sample,
+	    image: File.new(fixtures_path.join( rand('photographer.jpg', 'lady_bird.jpg', 'sunrise.jpg', 'waterfall.jpg'))
+
+end
+
+10.times do
+	uc = Ucomment.new
+	uc.body = Faker::Lorem.sentence(1)
+	uc.photo_id = rand(1..10)
+	uc.user_id = rand(1..11)
+	uc.score = rand(1..5)
+	uc.save
+end
