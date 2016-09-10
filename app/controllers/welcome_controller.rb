ed5ca01446340417eaae09ac3c559b0ff09d3ca9
Vasily Toropchin <vasview@gmail.com>
2016-09-10 18:51:32 +0600

    Some css styles were added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+  	@photos = Photo.all
   end
 
   def show
+  	@photo = Photo.find(params[:id])
+    @rating = @photo.ucomments.average(:score)
+  end
+
+  private
+
+  def welcome_params
+  	params.require(:photo).permit(:id, :caption, :user_id, :image)
   end
 end
